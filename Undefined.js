3f8920e7eda30382425f52e0e0d5d37b1eea6d52
arshad-turtle <arshad.bagwan@turtlemint.com>
2023-03-24 11:21:11 +0530

    Undefined Explained

---
+++ Undefined.js
@@ -0,0 +1,9 @@
+// Declaration and Defined
+
+var value
+
+//whats the value between this 2 line of variable value?  in JS as you declare a variable there is explicit value assgined / initilalized with UNDEFINED
+//What is UNDEFINED?  ---> IT is a JS datatype for example in boolean there is 2 value true or false so in undefined there is only 1 type
+// No type information
+console.log(value)
+value = 42;
