83f36767dac550d0c3852bcbb2ac73201694b301
arshad-turtle <arshad.bagwan@turtlemint.com>
2023-03-24 11:15:31 +0530

    JS Primitives

---
+++ app.js
@@ -0,0 +1,26 @@
+//Data Types --
+//Numbers --> Double Precision 64 but format iee 754 values
+//( No integers)
+
+var  a= 10;
+var b = 20
+c = a +b
+console.log(c);
+
+
+// String
+
+var s = 'Hello World'
+
+console.log(s);
+
+
+// Variable can hold value of any type can be called as Loose Typing
+//Boolean
+
+ a = true
+console.log(a)
+
+
+
+
